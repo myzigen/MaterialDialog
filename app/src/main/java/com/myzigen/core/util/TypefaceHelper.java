c4a12cbb18157f4b16b56a8b823ee9f652c280a1
myzigen <hafidzramandhani@gmail.com>
2023-12-11 10:33:49 +0700

    add pref

---
+++ app/src/main/java/com/myzigen/core/util/TypefaceHelper.java
@@ -0,0 +1,27 @@
+package com.myzigen.core.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import androidx.collection.SimpleArrayMap;
+
+public class TypefaceHelper {
+
+    private static final SimpleArrayMap<String, Typeface> cache = new SimpleArrayMap<>();
+
+    public static Typeface get(Context c, String name) {
+        synchronized (cache) {
+            if (!cache.containsKey(name)) {
+                try {
+                    Typeface t = Typeface.createFromAsset(
+                            c.getAssets(), String.format("fonts/%s", name));
+                    cache.put(name, t);
+                    return t;
+                } catch (RuntimeException e) {
+                    return null;
+                }
+            }
+            return cache.get(name);
+        }
+    }
+}
+
